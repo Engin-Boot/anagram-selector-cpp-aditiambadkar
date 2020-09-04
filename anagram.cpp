8bc80064ef4435dba595a98443583c45bb03bbe3
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-09-04 17:54:21 +0530

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -1,8 +1,19 @@
 #include "anagram.h"
+#include <bits/stdc++.h> 
 
 bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& word2) {
-    //Fill the correct implementation here
-    return false;
+    if(word1.length() != word2.length())
+        return false;
+    transform(word1.begin(), wordl.end(), wordl.begin(), ::tolower); 
+    transform(wordl.begin(), wordl.end(), wordl.begin(), ::tolower); 
+    sort(word1.begin(), word1.end());
+    sort(word2.begin(), word2.end());
+    for(int i = 0; i < word1.length(); i++)
+    {
+        if(word1[i] != word2[i])
+            return false;
+    }
+    return true;
 }
 
 std::vector<std::string> Anagram::SelectAnagrams(
