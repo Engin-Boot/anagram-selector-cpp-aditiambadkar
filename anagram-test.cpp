2d9f511944035621e0b67999af27435473d4b9bf
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-09-06 21:00:54 +0530

    Update anagram-test.cpp

--- anagram-test.cpp
+++ anagram-test.cpp
@@ -17,6 +17,11 @@ TEST_CASE("recognizes anagrams when there are case- and space- differences") {
         == true);
 }
 
+TEST_CASE("recognizes anagrams when there are special characters") {
+    REQUIRE(Anagram::WordPairIsAnagram("Church of Scientology", "rich-chosen goofy cult")
+        == true);
+}
+
 TEST_CASE("selects anagrams of a word") {
     auto selection = Anagram::SelectAnagrams("master",
         {"stream", "something", "maters"});
